3e3b3513ff21f8a2fdc28a4213ba417e49c7344b
novviaa <73828109+novianh@users.noreply.github.com>
2020-11-06 13:08:03 +0700

    upload

---
+++ codewar/script.js
@@ -0,0 +1,12 @@
+var summation = function (num) {
+    let total = 0;
+    for (i = 1; i < num; i++) {
+        total = total + 1;
+    }
+
+    return total;
+}
+
+
+
+console.log(summation(2), 3);
